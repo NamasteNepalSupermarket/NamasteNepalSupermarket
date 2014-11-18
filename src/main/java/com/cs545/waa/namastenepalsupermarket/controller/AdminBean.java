64194114ee30fd2ab75cc6d04c8d03442f89cfc0
Rupak <rupaknpl@gmail.com>
2014-11-18 14:36:18 -0600

    product initial work

--- src/main/java/com/cs545/waa/namastenepalsupermarket/controller/AdminBean.java
+++ src/main/java/com/cs545/waa/namastenepalsupermarket/controller/AdminBean.java
@@ -6,11 +6,13 @@
 package com.cs545.waa.namastenepalsupermarket.controller;
 
 import com.cs545.waa.namastenepalsupermarket.ejb.CategoryFacadeLocal;
+import com.cs545.waa.namastenepalsupermarket.ejb.ProductFacadeLocal;
 import com.cs545.waa.namastenepalsupermarket.model.Category;
 import com.cs545.waa.namastenepalsupermarket.model.Product;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.ejb.TransactionAttribute;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -26,7 +28,14 @@ public class AdminBean implements Serializable {
     private Product product;
     @EJB
     private CategoryFacadeLocal categoryFacadeLocal;
+    @EJB
+    private ProductFacadeLocal productFacadeLocal;
 
+    public AdminBean() {
+        product=new Product();
+        category=new Category();
+    }
+   
     public Category getCategory() {
         return category;
     }
@@ -55,4 +64,27 @@ public class AdminBean implements Serializable {
     public List<Category> getCategoryList() {
         return categoryFacadeLocal.findAll();
     }
+
+    public ProductFacadeLocal getProductFacadeLocal() {
+        return productFacadeLocal;
+    }
+
+    public void setProductFacadeLocal(ProductFacadeLocal productFacadeLocal) {
+        this.productFacadeLocal = productFacadeLocal;
+    }
+    
+    @TransactionAttribute
+    public void saveProduct(){
+        System.out.println("inside saveProduct");
+        productFacadeLocal.create(product);  
+        product=new Product();
+    }
+    
+    @TransactionAttribute
+    public void saveCategory(){
+        System.out.println("inside saveCategory");
+        categoryFacadeLocal.create(category);  
+        category=new Category();
+    }
+    
 }
