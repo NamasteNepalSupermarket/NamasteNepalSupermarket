016d425efdc0058fdf5de19a0f109a1e69c5cd0b
ishal <timibishal@gmail.com>
2014-11-19 15:05:20 -0500

    administrator login

--- src/main/java/com/cs545/waa/nns/model/Person.java
+++ src/main/java/com/cs545/waa/nns/model/Person.java
@@ -23,9 +23,12 @@ public class Person implements Serializable{
     private Long id;
     private String username;
     private String password;
-    private String firstname;
-    private String lastname;
+    private String fullname;
+
+    
     private String email;
+    private String address;
+    
 
     public Person() {
     }
@@ -37,7 +40,21 @@ public class Person implements Serializable{
     public void setId(Long id) {
         this.id = id;
     }
+public String getFullname() {
+        return fullname;
+    }
 
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     public String getUsername() {
         return username;
     }
@@ -54,21 +71,7 @@ public class Person implements Serializable{
         this.password = password;
     }
 
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+    
 
     public String getEmail() {
         return email;
